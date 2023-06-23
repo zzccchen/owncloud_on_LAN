b17fcde5d78148d78b25527dcfb153f388f46b7f
zzccchen <zzccchen@qq.com>
2023-06-23 23:27:00 +0800

    Update owncloud_server10.bash

--- owncloud_server10.bash
+++ owncloud_server10.bash
@@ -1,27 +1,26 @@
-#!/bin/bash    
+#!/bin/bash
 
 apt update  # To get the latest package lists
 apt install apache2 mariadb-server -y
 apt install curl gnupg2 -y
 
 # sudo add-apt-repository ppa:ondrej/php
-nano /etc/apt/sources.list
-echo "deb https://ppa.launchpadcontent.net/ondrej/php/ubuntu jammy main" >> /etc/apt/sources.list
-echo "#deb-src https://ppa.launchpadcontent.net/ondrej/php/ubuntu jammy main " >> /etc/apt/sources.list
+echo "deb https://ppa.launchpadcontent.net/ondrej/php/ubuntu jammy main" | tee -a /etc/apt/sources.list
+echo "#deb-src https://ppa.launchpadcontent.net/ondrej/php/ubuntu jammy main " | tee -a /etc/apt/sources.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 
 apt update
 apt install php7.4
 php7.4 -m
-apt-cache search php7.4-mysql
-apt install php7.4-mysql
-apt install php-pear
-apt install mariadb-server 
-apt install php7.4 libapache2-mod-php7.4 php7.4-{mysql,intl,curl,json,gd,xml,mbstring,zip} -y
-echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/server:/10.9.1/Ubuntu_22.04/ /' > /etc/apt/sources.list.d/isv:ownCloud:server:10.list
-curl -fsSL https://download.opensuse.org/repositories/isv:ownCloud:server:10/Ubuntu_20.04/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/isv_ownCloud_server_10.gpg
+# apt-cache search php7.4-mysql
+apt install php7.4-mysql php-pear -y
+apt install  libapache2-mod-php7.4 php7.4-{intl,curl,json,gd,xml,mbstring,zip} -y
+
+echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/server:/10/Ubuntu_22.04/ /' > /etc/apt/sources.list.d/isv:ownCloud:server:10.list
+curl -fsSL https://download.opensuse.org/repositories/isv:ownCloud:server:10/Ubuntu_22.04/Release.key | gpg --dearmor > /etc/apt/trusted.gpg.d/isv_ownCloud_server_10.gpg
 apt update
 apt install owncloud-complete-files -y
+
 mkdir /var/www/owncloud
 cat > /etc/apache2/sites-available/owncloud.conf << 'EOL'
 Alias / "/var/www/owncloud/"
@@ -45,17 +44,19 @@ a2dissite 000-default.conf
 a2enmod rewrite mime unique_id
 apachectl -t
 systemctl restart apache2
-mysql --password=1234 --user=root --host=localhost << eof
+
+mysql --password=zzccchen1234 --user=root --host=localhost << eof
 create database ownclouddb;
-grant all privileges on ownclouddb.* to root@localhost identified by "1234";
+grant all privileges on ownclouddb.* to root@localhost identified by "zzccchen1234";
 flush privileges;
 exit;
 eof
+
 cd /var/www/owncloud
 sudo -u www-data php occ maintenance:install \
    --database "mysql" \
    --database-name "ownclouddb" \
    --database-user "root"\
-   --database-pass "1234" \
+   --database-pass "zzccchen1234" \
    --admin-user "root" \
-   --admin-pass "1234"
+   --admin-pass "zzccchen1234"
